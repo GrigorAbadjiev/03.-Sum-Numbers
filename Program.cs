fdf75dc15fc7fcb599e27e6d5bd3a22909ac8a74
User <User@DESKTOP-ESP2JT1>
2022-11-30 16:27:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goalNumber = int.Parse(Console.ReadLine());
+
+            int sumOfNumber = 0;
+
+            while (sumOfNumber < goalNumber)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+
+                sumOfNumber += currentNumber;
+            }
+
+            Console.WriteLine(sumOfNumber);
+        }
+    }
+}
